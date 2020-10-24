a9498321bd582496a96793ee96c9899579c0a5c8
TurboHz <turbohz@gmail.com>
2020-10-24 18:38:40 +0200

    Adds target=_self to attributes that prevent link interception

--- runtime/src/app/router/index.ts
+++ runtime/src/app/router/index.ts
@@ -135,7 +135,8 @@ function handle_click(event: MouseEvent) {
 	// Ignore if tag has
 	// 1. 'download' attribute
 	// 2. rel='external' attribute
-	if (a.hasAttribute('download') || a.getAttribute('rel') === 'external') return;
+	// 3. target='self' attribute
+	if (a.hasAttribute('download') || a.getAttribute('rel') === 'external' || a.getAttribute('target') === '_self') return;
 
 	// Ignore if <a> has a target
 	if (svg ? (<SVGAElement>a).target.baseVal : a.target) return;
